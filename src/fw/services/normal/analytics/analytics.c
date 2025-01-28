423a462ce016ce5f523b7846610817ef10f4fd8d
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-01-28 11:59:14 -0500

    spelling: increment
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/fw/services/normal/analytics/analytics.c
+++ src/fw/services/normal/analytics/analytics.c
@@ -184,7 +184,7 @@ void analytics_stopwatch_stop(AnalyticsMetric metric) {
 
   AnalyticsStopwatchNode *stopwatch = prv_find_stopwatch(metric);
   if (!stopwatch) {
-    // TODO: Incerement this back up to LOG_LEVEL_WARNING when it doesn't happen
+    // TODO: Increment this back up to LOG_LEVEL_WARNING when it doesn't happen
     // on every bootup (PBL-5393)
     PBL_LOG(LOG_LEVEL_DEBUG, "Analytics stopwatch for metric %d already stopped!", metric);
     goto unlock;
